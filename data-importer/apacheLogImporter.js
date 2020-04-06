eff393c9adac3d4623114aed7a59dd3dd61a40e0
Johnny Oshika <johnnyoshika@gmail.com>
2020-04-06 11:09:54 -0700

    Fix bug in apacheLogImporter

--- data-importer/apacheLogImporter.js
+++ data-importer/apacheLogImporter.js
@@ -1,32 +1,33 @@
-var fs = require('fs');
-var Alpine = require('alpine');
-var alpine = new Alpine();
+const fs = require('fs');
+const Alpine = require('alpine');
+const alpine = new Alpine();
+const moment = require('moment');
 const bulk = require('./bulk');
 
 const apacheLogImporter = async function () {
-  let logs = [];
-  let firstImport = true;
-
-  alpine.parseReadStream(
-    fs.createReadStream('../apache-logs-data/access_log', {
-      encoding: 'utf8',
-    }),
-    async data => {
-      // There's a bug here. The index resets sporadically and it seems to go on forever.
-      // When the count gets to about 167,402 (which it never should b/c there are only 20K records, but it seems to max out at 167,402), stop the script.
-      logs.push(data);
-      if (logs.length > 1000) {
-        await bulk({
-          index: 'apache_logs',
-          mappings: {},
-          dataset: logs,
-          reset: firstImport,
-        });
-        logs = [];
-        firstImport = false;
-      }
-    },
-  );
+  fs.readFile('../apache-logs-data/access_log', async (err, data) => {
+    if (err) throw err;
+    const lines = data
+      .toString()
+      .split('\n')
+      .filter(l => l.trim() !== '');
+    await bulk({
+      index: 'apache_logs',
+      mappings: {
+        properties: {
+          time: { type: 'date' },
+        },
+      },
+      dataset: lines.map(line => {
+        const log = alpine.parseLine(line);
+        return {
+          ...log,
+          time: moment(log.time, 'DD/MMM/YYYY:HH:mm:ss Z').format(),
+        };
+      }),
+      reset: true,
+    });
+  });
 };
 
 module.exports = apacheLogImporter;
